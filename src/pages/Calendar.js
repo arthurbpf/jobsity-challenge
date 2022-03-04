6a3fc7f27d191e36c4353b7f388311b50dc8cad9
Arthur Fernandes <arthurbpfernandes@gmail.com>
2022-03-04 19:22:53 -0300

    Initial setup

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 function Calendar(props) {
-  // your calendar implementation Goes here!
-  // Be creative 
   return (
     <div className="container">
         <h1>Calendar</h1>
